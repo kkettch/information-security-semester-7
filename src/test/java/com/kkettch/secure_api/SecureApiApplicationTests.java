5134a366b52047b700a6ef830701e95ccdc45e94
kkettch <369027@niuitmo.ru>
2025-09-24 12:56:47 +0300

    initializing commit

---
+++ src/test/java/com/kkettch/secure_api/SecureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkettch.secure_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
